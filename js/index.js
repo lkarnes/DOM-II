e07ad6013c683931ef4017c1f2f821f812cff8ed
Logan Karnes <logan.karnes98@gmail.com>
2019-06-06 21:24:53 -0700

    2/3

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+const navLink = document.querySelectorAll('.nav-link');
+
+navLink.forEach( (item, i) => {
+    navLink[i].addEventListener('click', (event) => {
+        navLink[i].style.background = 'red';
+        navLink[i].style.fontSize = '2rem';
+    }
+)})
+
+const logo = document.querySelector('.logo-heading');
+
+logo.addEventListener('mouseover', () =>{
+        logo.style.fontSize = '6rem';
+})
